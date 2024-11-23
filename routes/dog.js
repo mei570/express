a3fb2649637a664d822ebb345afb33b54009d32e
仲底芽衣 <nakasokomei@icloud.com>
2024-11-23 19:02:08 +0900

    add dog api

--- routes/dog.js
+++ routes/dog.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example', function (error, response,
+  //ランダムに犬の画像を取得するAPI
+request('https://dog.ceo/api/breeds/image/random', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
